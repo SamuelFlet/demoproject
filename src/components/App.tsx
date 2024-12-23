d3abfc248276f9fce1829834ed9ff82d8ef01a7d
Samuel Fletcher <samuelf@wingmateapp.com>
2024-12-23 10:32:50 -0500

    creating thing with lots of errors

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,21 +1,17 @@
 import React from 'react';
-import './App.css';
+import logo from '../img/logo512.png';
+import { Button } from '@mui/material';
+import '../styles/App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <img src={logo} className="App-logo"/>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <Button variant='contained'>Hello World</Button>
       </header>
     </div>
   );
